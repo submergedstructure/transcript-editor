d9f443c9e0903d9681911689f8043056670bfb57
James Pratt <me@jamiep.org>
2024-08-20 22:39:41 +0200

    morph popup #1

--- dev/morph_tree_parser.clj
+++ dev/morph_tree_parser.clj
@@ -1,15 +1,24 @@
 (ns morph-tree-parser
   (:require [clojure.string :as str]))
 
+;; Examinining possible attributes and values that can be returned by spacy for Polish language.
+
+;; from https://spacy.io/models/pl Morphologizer section
+;; see also https://universaldependencies.org/pl/index.html for more information
+
 (def morph-tree (slurp "resources/morpholiger_leaf_tree.txt"))
 
 (defn morph-tree-parser
-  "Parse the morpholiger leaf tree"
+  "Parse the morpholiger leaf tree. Returns a vector of maps, where each map
+   represents a leaf in the tree. Each leaf is a map of attributes and values."
   []
   (->> (str/split morph-tree #"\, ")
        (map (fn [leaf] (str/split leaf #"\|")))
        (map (fn [attrs-and-values]
-              (map (fn [attr-and-value] (str/split attr-and-value #"=")) attrs-and-values)))
+              (map (fn [attr-and-value]
+                     (let [[k v](str/split attr-and-value #"=")]
+                       [k v]))
+                   attrs-and-values)))
        (mapv #(into {} %))))
 
 (defn morph-tree-attributes-and-values
@@ -18,16 +27,37 @@
   (distinct
    (apply concat
           (for [leaf (morph-tree-parser)
-                :when (not (#{"PUNCT" "SYM"} (get leaf "POS")))]
+                :when (not (#{"PUNCT"} (get leaf "POS")))]
             (for [[attr value] leaf
                   :when (not= attr "POS")]
               [attr value])))))
 
+(defn morph-tree-attributes-and-values-per-pos []
+  (group-by #(get % "POS") (morph-tree-parser)))
+
+(defn morph-tree-attributes-and-values-per-pos-groups-with-same-attributes
+  "For each part of speech a collection of maps of unique combinations of attributes and the possible values these attributes can take."
+  []
+  (apply merge-with into
+        (apply
+         concat
+         (for [[pos leaves] (morph-tree-attributes-and-values-per-pos)]
+           (->> leaves
+                (group-by #(sort (keys %)))
+                (map (fn [[_ks samekeys]] (map (fn [leaf] (into {} (map (fn [[k v]] [k #{v}])) leaf)) samekeys)))
+                (map (fn [samekeys] {(keyword pos) [(apply merge-with into samekeys)]})))))))
 
 (comment 
   (morph-tree-parser)
   (println (apply str (for [[attr value] (morph-tree-attributes-and-values)]
     (str attr " = " value "\n"))))
   (count *1)
+
+  
+  (:NOUN (morph-tree-attributes-and-values-per-pos-groups-with-same-attributes))
+  (count *1)
+  (keys *2)
+
+  (morph-tree-attributes-and-values-per-pos-groups-with-same-attributes)
   )
 
--- src/com/submerged_structure/components/word_with_morphological_features_popup.cljs
+++ src/com/submerged_structure/components/word_with_morphological_features_popup.cljs
@@ -1,7 +1,7 @@
 (ns com.submerged-structure.components.word-with-morphological-features-popup
   (:require
-   [com.fulcrologic.fulcro.dom :as dom  :refer [div p span a]]
-   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+   [com.fulcrologic.fulcro.dom :as dom  :refer [h3 div p span a iframe]]
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc fragment]]
    [com.fulcrologic.semantic-ui.modules.popup.ui-popup-content :refer [ui-popup-content]]
    [com.fulcrologic.semantic-ui.elements.label.ui-label :refer [ui-label]]
    [com.fulcrologic.semantic-ui.elements.label.ui-label-detail :refer [ui-label-detail]]
@@ -22,115 +22,110 @@
 ;; see also https://universaldependencies.org/pl/index.html for more information
 
 (def map-of-attribute-names-to-human-readable-name
-  {"AdpType" "Adposition Type"
-   "VerbForm" "Verb Form"
-   "PunctType" "Punctuation Type"
-   "PronType" "Pronoun Type"
-   "NumForm" "Numeral Form"
-   "PrepCase" "Prepositional Case"
-   "Reflex" "Reflexive"
-   "NumType" "Numeral Type"
-   "Hyph" "Hyphenated"
-   "Poss" "Possessive"
-   "ConjType" "Conjunction Type"
-   "VerbType" "Verb Type"
-   "Number[psor]" "Possessor's Number"
-   "PartType" "Particle Type"
-   "Abbr" "Abbreviation"
-   "Pun" "Punctuation"
-   "PunctSide" "Punctuation Side"
-   "Polite" "Politeness"})
+  {"AdpType" "adposition type"
+   "VerbForm" "verb form"
+   "PunctType" "punctuation type"
+   "PronType" "pronoun type"
+   "NumForm" "numeral form"
+   "PrepCase" "prepositional case"
+   "Reflex" "reflexive"
+   "NumType" "numeral type"
+   "Hyph" "hyphenated"
+   "Poss" "possessive"
+   "ConjType" "conjunction type"
+   "VerbType" "verb type"
+   "Number[psor]" "possessor's number"
+   "PartType" "particle type"
+   "Abbr" "abbreviation"
+   "Pun" "punctuation"
+   "PunctSide" "punctuation side"
+   "Polite" "politeness"})
 
 (def map-of-attribute-name-and-value-to-human-readable-value
-  {["Abbr" "Yes"] "Abbreviation"
-   ["AdpType" "Post"] "Postposition"
-   ["AdpType" "Prep"] "Preposition"
-   ["Animacy" "Hum"] "Human"
-   ["Animacy" "Inan"] "Inanimate"
-   ["Animacy" "Nhum"] "Non-human"
-   ["Aspect" "Imp"] "Imperfective"
-   ["Aspect" "Imp,Perf"] "Imperf and Perf identical "
-   ["Aspect" "Perf"] "Perfective"
-   ["Case" "Acc"] "Accusative"
-   ["Case" "Dat"] "Dative"
-   ["Case" "Gen"] "Genitive"
-   ["Case" "Ins"] "Instrumental"
-   ["Case" "Loc"] "Locative"
-   ["Case" "Nom"] "Nominative"
-   ["Case" "Voc"] "Vocative"
-   ["Clitic" "Yes"] "Clitic"
-   ["ConjType" "Comp"] "Complementizer"
-   ["ConjType" "Oper"] "Operative"
-   ["ConjType" "Pred"] "Predicate"
-   ["Degree" "Cmp"] "Comparative"
-   ["Degree" "Pos"] "Positive"
-   ["Degree" "Sup"] "Superlative"
-   ["Emphatic" "Yes"] "Emphatic"
-   ["Foreign" "Yes"] "Foreign"
-   ["Gender" "Fem"] "Feminine"
-   ["Gender" "Masc"] "Masculine"
-   ["Gender" "Neut"] "Neuter"
-   ["Hyph" "Yes"] "Hyphenated"
-   ["Mood" "Imp"] "Imperative"
-   ["Mood" "Ind"] "Indicative"
-   ["Number" "Plur"] "Plural"
-   ["Number" "Plur,Sing"] "Plural and Singular Identical"
-   ["Number" "Ptan"] "Always plural"
-   ["Number" "Sing"] "Singular"
-   ["Number[psor]" "Plur"] "Plural possessor"
-   ["Number[psor]" "Sing"] "Singular possessor"
-   ["NumForm" "Digit"] "Digits"
-   ["NumForm" "Roman"] "Roman numerals"
-   ["NumForm" "Word"] "Word"
-   ["NumType" "Card"] "Cardinal numeral"
-   ["NumType" "Ord"] "Ordinal numeral"
-   ["NumType" "Sets"] "Refers to a set"
-   ["PartType" "Int"] "Interjectional particle"
-   ["PartType" "Mod"] "Modal particle"
+  {["Abbr" "Yes"] "abbreviation"
+   ["AdpType" "Post"] "postposition"
+   ["AdpType" "Prep"] "preposition"
+   ["Animacy" "Hum"] "human"
+   ["Animacy" "Inan"] "inanimate"
+   ["Animacy" "Nhum"] "non-human"
+   ["Aspect" "Imp"] "imperfective"
+   ["Aspect" "Imp,Perf"] "imperf and perf identical"
+   ["Aspect" "Perf"] "perfective"
+   ["Case" "Acc"] "accusative"
+   ["Case" "Dat"] "dative"
+   ["Case" "Gen"] "genitive"
+   ["Case" "Ins"] "instrumental"
+   ["Case" "Loc"] "locative"
+   ["Case" "Nom"] "nominative"
+   ["Case" "Voc"] "vocative"
+   ["Clitic" "Yes"] "clitic"
+   ["ConjType" "Comp"] "complementizer"
+   ["ConjType" "Oper"] "operative"
+   ["ConjType" "Pred"] "predicate"
+   ["Degree" "Cmp"] "comparative"
+   ["Degree" "Pos"] "positive"
+   ["Degree" "Sup"] "superlative"
+   ["Emphatic" "Yes"] "emphatic"
+   ["Foreign" "Yes"] "foreign"
+   ["Gender" "Fem"] "feminine"
+   ["Gender" "Masc"] "masculine"
+   ["Gender" "Neut"] "neuter"
+   ["Hyph" "Yes"] "hyphenated"
+   ["Mood" "Imp"] "imperative"
+   ["Mood" "Ind"] "indicative"
+   ["Number" "Plur"] "plural"
+   ["Number" "Plur,Sing"] "plural and singular identical"
+   ["Number" "Ptan"] "always plural"
+   ["Number" "Sing"] "singular"
+   ["Number[psor] " "Plur"] "plural possessor"
+   ["Number[psor]" "Sing"] "singular possessor"
+   ["NumForm" "Digit"] "digits"
+   ["NumForm" "Roman"] "roman numerals"
+   ["NumForm" "Word"] "word"
+   ["NumType" "Card"] "cardinal numeral"
+   ["NumType" "Ord"] "ordinal numeral"
+   ["NumType" "Sets"] "refers to a set"
+   ["PartType" "Int"] "interjectional particle"
+   ["PartType" "Mod"] "modal particle"
    ["Person" "0"] "impersonal"
    ["Person" "1"] "1st person"
    ["Person" "1,2"] "1st or 2nd person"
-   ["Person" "2"] "Second person"
+   ["Person" "2"] "2nd person"
    ["Person" "3"] "3rd person"
-   ["Polarity" "Neg"] "Negative"
-   ["Polarity" "Pos"] "Positive"
-   ["Polite" "Depr"] "Derogatory"
-   ["Poss" "Yes"] "Possessive"
-   ["PrepCase" "Npr"] "Non-prepositional"
-   ["PrepCase" "Pre"] "Prepositional"
-   ["PronType" "Dem"] "Demonstrative"
-   ["PronType" "Ind"] "Indefinite"
-   ["PronType" "Int"] "Interrogative"
-   ["PronType" "Neg"] "Negative"
-   ["PronType" "Prs"] "Personal"
-   ["PronType" "Rel"] "Relative"
-   ["PronType" "Tot"] "Total"
-   ["Pun" "No"] "No punctuation"
-   ["Pun" "Yes"] "Punctuation"
-   ["Reflex" "Yes"] "Reflexive"
-   ["Tense" "Fut"] "Future"
-   ["Tense" "Past"] "Past"
-   ["Tense" "Pres"] "Present"
-   ["Variant" "Long"] "Long"
-   ["Variant" "Short"] "Short"
-   ["VerbForm" "Conv"] "Converb"
-   ["VerbForm" "Fin"] "Finite verb"
-   ["VerbForm" "Inf"] "Infinitive"
-   ["VerbForm" "Part"] "Participle"
-   ["VerbForm" "Vnoun"] "Verbal noun"
-   ["VerbType" "Mod"] "Modal verb"
-   ["VerbType" "Quasi"] "Quasi-verb (no person, aspect, number)"
-   ["Voice" "Act"] "Active"
-   ["Voice" "Pass"] "Passive"})
+   ["Polarity" "Neg"] "negative"
+   ["Polarity" "Pos"] "positive"
+   ["Polite" "Depr"] "derogatory"
+   ["Poss" "Yes"] "possessive"
+   ["PrepCase" "Npr"] "non-prepositional"
+   ["PrepCase" "Pre"] "prepositional"
+   ["PronType" "Dem"] "demonstrative"
+   ["PronType" "Ind"] "indefinite"
+   ["PronType" "Int"] "interrogative"
+   ["PronType" "Neg"] "negative"
+   ["PronType" "Prs"] "personal"
+   ["PronType" "Rel"] "relative"
+   ["PronType" "Tot"] "total"
+   ["Pun" "No"] "no punctuation"
+   ["Pun" "Yes"] "punctuation"
+   ["Reflex" "Yes"] "reflexive"
+   ["Tense" "Fut"] "future"
+   ["Tense" "Past"] "past"
+   ["Tense" "Pres"] "present"
+   ["Variant" "Long"] "long"
+   ["Variant" "Short"] "short"
+   ["VerbForm" "Conv"] "converb"
+   ["VerbForm" "Fin"] "finite verb"
+   ["VerbForm" "Inf"] "infinitive"
+   ["VerbForm" "Part"] "participle"
+   ["VerbForm" "Vnoun"] "verbal noun"
+   ["VerbType" "Mod"] "modal verb"
+   ["VerbType" "Quasi"] "quasi-verb (no person, aspect, number)"
+   ["Voice" "Act"] "active"
+   ["Voice" "Pass"] "passive"})
 
 
 
-(defn dict-link-instructions [is_morphed]
-  (if is_morphed
-    (p "Click on either the linked word above or it's root, to open dictonary for conjugated word or it's root entry.")
-    (p "Click on the linked word above to open dictonary entry.")))
-
-(defn attribute-name-to-human-readable-name
+(defn human-readable-attribute-name
   [attribute-name]
   (if-let [hr-name (get map-of-attribute-names-to-human-readable-name attribute-name)]
     hr-name
@@ -138,10 +133,11 @@
 
 (defn human-readable-attribute-value
   "need attribute name as context"
-  [attribute-name attribute-value]
-  (if-let [hr-name (get map-of-attribute-name-and-value-to-human-readable-value [attribute-name attribute-value])]
+  [morph-map attribute-name]
+  (let [attribute-value (get morph-map attribute-name)]
+    (if-let [hr-name (get map-of-attribute-name-and-value-to-human-readable-value [attribute-name attribute-value])]
     hr-name
-    attribute-value))
+    attribute-value)))
 
 
 (defn morphological-features-str-to-map [morph-string]
@@ -155,17 +151,17 @@
   ;; => {"Animacy" "Inan", "Case" "Nom", "Gender" "Masc", "Number" "Sing"}
 )
 
-(defn morph-label [[attribute-name attribute-value] & [options]]
+(defn morph-label [morph-map attribute-name & {:keys [detail-options postfix prefix]}]
   (ui-label
-   (if options options {})
-   (attribute-name-to-human-readable-name attribute-name)
-   (ui-label-detail {} (human-readable-attribute-value attribute-name attribute-value))))
+   (if detail-options detail-options {})
+   (human-readable-attribute-name attribute-name)
+   (ui-label-detail {} (human-readable-attribute-value morph-map attribute-name))))
 
 
-(defn label-list-from-morphological-features-map-noun [{:strs [Animacy Case Gender Number NumType Aspect VerbForm Polarity Polite]}]
+(defn label-list-from-morphological-features-map-noun [{:strs [Animacy Case Gender Number NumType Aspect VerbForm Polarity Polite] :as morph-map}]
   (div
-   (when Polite (morph-label ["Polite" Polite] {:color "red"}))
-   (morph-label ["Case" Case])
+   (when Polite (morph-label morph-map "Polite" {:detail-options {:color "red"}}))
+   (morph-label morph-map "Case")
    (ui-label {}
              "Gender"
              (ui-label-detail {} (str (human-readable-attribute-value "Gender" Gender)
@@ -188,24 +184,50 @@
                           ")")))))))
 
 
-(defn html-from-morphological-features-map [pos {:as morph-map}]
-  (case pos
-    "NOUN" (label-list-from-morphological-features-map-noun morph-map)
-    "PROPN" (label-list-from-morphological-features-map-noun morph-map)
-   (map morph-label morph-map)))
+(defn ui-morphological-features [pos {:as morph-map}]
+  (map (fn [[attribute-name _]] (morph-label morph-map attribute-name)) morph-map))
 
-(comment (html-from-morphological-features-map "NOUN" "Animacy=Inan|Case=Nom|Gender=Masc|Number=Sing")
+(comment (ui-morphological-features "NOUN" "Animacy=Inan|Case=Nom|Gender=Masc|Number=Sing")
 )
 
+(defn human-readable-parts-with-spacing [morph-map attribute-names]
+  (str/join " "
+            (for [attr attribute-names
+                  :let [value (get morph-map attr)]
+                  :when value]
+              (human-readable-attribute-value morph-map attr))))
 
+(defn case-gender-number [{:strs [Animacy Case Gender] :as morph-map}]
+  (str/capitalize
+   (str (when Case (str (human-readable-attribute-value morph-map "Case") " "))
+        
+        (when Gender (human-readable-attribute-value morph-map "Gender"))
 
-(defn dict-link
+        (when Animacy (str " (" (human-readable-attribute-value morph-map "Animacy") ")"))
+        " "
+        (when Case (human-readable-attribute-value morph-map "Number")))))
+
+(defn word-class [pos pos_explained morph-map]
+  (case pos
+    ("NOUN" "PROPN" "ADJ" "PRON") (str (case-gender-number morph-map) " " pos_explained)
+    "ADP" (str (human-readable-attribute-value morph-map "AdpType"))
+    (str pos_explained)))
+
+(defn ui-dict-link-and-popup
   "More than one link for when word is two space separated lemmas."
   [word]
-  (for [ws (str/split word #" ")]
-    (span (a {:href (str "https://www.diki.pl/slownik-angielskiego?q=" ws)
-              :target "_blank"}
-             ws) " ")))
+  (for [w (str/split word #" ")]
+    (fragment
+     (ui-popup
+      {:trigger
+
+       (a {:href (str "https://www.diki.pl/slownik-angielskiego?q=" w)
+           :target "_blank"}
+          w)}
+      (ui-popup-content
+       {}
+       (str "Click to open dictionary entry for \"" w "\".")))
+     " ")))
 
 (defsc WordWithMorphPopup [_this {:word/keys [word active score start
                                 morph lemma pos pos_explained is_morphed norm]}]
@@ -214,9 +236,12 @@
    :query [:word/id :word/word :word/start :word/end :word/active :word/score
            :word/morph :word/lemma :word/pos :word/pos_explained :word/is_morphed :word/norm]}
   (ui-popup
-   {:size "tiny"
-    :hoverable true
-    :position "bottom center"
+   {:hoverable true
+
+    :popperModifiers #js [#js {"name" "preventOverflow"
+                               "options" #js {"padding" 8
+                                              "RootBoundary" "viewport"}}]
+    :position "bottom right"
     :hideOnScroll true
     :trigger (span {:data-c score
                     :classes [(when active "active") "word"]
@@ -227,20 +252,15 @@
 
    (ui-popup-content
     {}
-    (div :.ui.raised.segment
-         (div :.ui.red.ribbon.label pos_explained)
-         (span nil
-               (when is_morphed
-                 (span nil (dict-link lemma) "  " (ui-icon {:name i/arrow-right-icon}) " "))
-               (dict-link norm))
-         (p "")
-         (div :.ui.blue.ribbon.label "Community")
-         "User Reviews"
-         (p "")
+    (div :.ui.red.ribbon.label (word-class pos pos_explained (morphological-features-str-to-map morph)))
+    (h3 nil
+        (when is_morphed
+          (fragment {} (ui-dict-link-and-popup lemma) "  " (ui-icon {:name i/arrow-right-icon}) " "))
+        (ui-dict-link-and-popup norm))
+
+    (div
      (if (not-empty morph)
-      (div
-       (dict-link-instructions is_morphed)
-       (html-from-morphological-features-map pos (morphological-features-str-to-map morph)))
-      (dict-link-instructions is_morphed))))))
+       (ui-morphological-features pos (morphological-features-str-to-map morph))
+       "This word only has one form! No inflection! Phew!!")))))
 
 (def ui-word-with-morph-popup (comp/factory WordWithMorphPopup {:keyfn :word/id}))
