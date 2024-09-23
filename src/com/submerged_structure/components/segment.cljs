90dcd1f2bbae8852412dda5930b9dd858101c4e1
James Pratt <me@jamiep.org>
2024-09-23 10:57:28 +0200

    renaming WordMorphologicalInfoGrid to WordMorphologicalInfo

--- src/com/submerged_structure/components/segment.cljs
+++ src/com/submerged_structure/components/segment.cljs
@@ -4,20 +4,20 @@
             [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
             [com.submerged-structure.components.translation :as translation]
             [com.submerged-structure.components.word :as word-with-morphological-features-popup]
-            [com.submerged-structure.components.segment-morphological-info-grid :as word-morphological-info-grid]))
+            [com.submerged-structure.components.segment-morphological-info :as word-morphological-info-grid]))
 
 
 (defsc Segment [_this {:segment/keys [words translations text] :>/keys [morphological-info-grid]} computed]
   {:ident :segment/id
    :initial-state (fn [_] {:segment/words (comp/get-initial-state word-with-morphological-features-popup/Word {})
                            :segment/translations (comp/get-initial-state translation/Translation {})
-                           :>/morphological-info-grid (comp/get-initial-state word-morphological-info-grid/WordMorphologicalInfoGrid {})})
+                           :>/morphological-info-grid (comp/get-initial-state word-morphological-info-grid/WordMorphologicalInfo {})})
    :query [:segment/id :segment/start :segment/end :segment/text
            {:segment/words (comp/get-query word-with-morphological-features-popup/Word)}
            {:segment/translations (comp/get-query translation/Translation)}
-           {:>/morphological-info-grid (comp/get-query word-morphological-info-grid/WordMorphologicalInfoGrid)}]}
+           {:>/morphological-info-grid (comp/get-query word-morphological-info-grid/WordMorphologicalInfo)}]}
   (div :.segment-transcription-and-translation
-       (word-morphological-info-grid/ui-word-morphological-info-grid morphological-info-grid)
+       (word-morphological-info-grid/ui-word-morphological-info morphological-info-grid)
        (span :.transcription
              (interleave
               (map #(word-with-morphological-features-popup/ui-word % computed) words) (repeat " ")))
--- src/com/submerged_structure/components/segment_morphological_info.cljs
+++ src/com/submerged_structure/components/segment_morphological_info.cljs
@@ -1,4 +1,4 @@
-(ns com.submerged-structure.components.segment-morphological-info-grid
+(ns com.submerged-structure.components.segment-morphological-info
   (:require
    [com.fulcrologic.fulcro.dom :as dom  :refer [div]]
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
@@ -7,11 +7,11 @@
    
    [com.submerged-structure.components.word-morphological-info :as word-morphological-info]))
 
-(defsc WordMorphologicalInfoGrid [_this {:segment/keys [words]}]
+(defsc WordMorphologicalInfo [_this {:segment/keys [words]}]
   {:ident :segment/id
    :query [:segment/id :segment/words {:segment/words (comp/get-query word-morphological-info/WordMorphologicalInfo)}]}
   (div :.ui.grid.stackable
-       (div :.doubling.ten.column.row
+       (div :.eight.column.row
             (map (fn [morphological-info] (word-morphological-info/ui-word-morphological-info morphological-info)) words))))
 
-(def ui-word-morphological-info-grid (comp/factory WordMorphologicalInfoGrid {:keyfn :segment/id}))
+(def ui-word-morphological-info (comp/factory WordMorphologicalInfo {:keyfn :segment/id}))
