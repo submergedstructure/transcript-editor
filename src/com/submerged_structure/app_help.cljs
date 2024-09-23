15d204dd9122dd0f0d960680157a4a10286b01ef
James Pratt <me@jamiep.org>
2024-09-23 14:30:19 +0200

    update app help

--- src/com/submerged_structure/app_help.cljs
+++ src/com/submerged_structure/app_help.cljs
@@ -1,13 +1,12 @@
 (ns com.submerged-structure.app-help
-  (:require [com.fulcrologic.fulcro.dom :as dom  :refer [h2 h3 h3 div li p ul b a]]
-            [com.fulcrologic.semantic-ui.collections.message.ui-message-header :refer [ui-message-header]]
+  (:require [com.fulcrologic.fulcro.dom :as dom  :refer [h2 h3 h3 div p b a]]
             [com.fulcrologic.fulcro.components :as comp :refer [fragment]]))
 
 
 (def app-help
   (fragment
    (h2 "How to use this player")
-   (p "As the audio plays you will see the currently spoken word is highlighed.")
+   (p "As the audio plays you will see the currently spoken word is highlighted.")
    (h3 "Several transcripts available: ")
    (p "At the very top of the page the heading with the transcript name is actually a dropdown menu, you can use this to switch between different transcripts.")
 
@@ -15,7 +14,7 @@
    (div
         (p (b "\"Plain Transcript - No Coloring\"") " for no formatting. Click on any word to go to that word and begin playing.")
         (p (b "\"AI's Confidence of Each Word\"") " colors each word to show the AI's confidence of the transcription of that word. Click on any word to go to that word and begin playing.")
-        (p (b "\"Grammar X-Ray\"") " formats text to show the grammatical features of each word (the default option). Click on any word for more info."))
+        (p (b "\"Grammar X-Ray\"") " (the default option) colours text to show the most important grammatical features of each word. Click on any word for more info."))
    (h3 "Playback control: ")
    (div
     (p "Hover over playback control buttons below the waveform with your cursor for more information about what they do.")
@@ -27,8 +26,10 @@
         (p "Click on the button next to a sentence to see a translation of that sentence. You can also click on the translation to hide it again.")
         (p "You can also click on the button to the right of the player controls to show or hide all translations at once."))
    (h3 "Grammar Analysis:")
-   (p "When in the \"Grammar X-Ray\" view you can click any word to see the root word it is derived from, what class of word it is and the grammatical features of that word.")
-   (h3 "Links to dictionary for individual words:")
+   (p "When in the \"Grammar X-Ray\" view you can click any word to see the root word it is derived from.")
+   (p "You can also click the eye button on the far right of the player controls to see all root words revealed.")
+   (p "If you hover over the box  showing the root word, or on a touch device click on the box, a popup will further break down the grammatical features of the word.")
+   (h3 "Links to dictionary for definitions of individual words:")
    (p "Click on linked root word and / or the declined word itself in the grammatical analysis pop up to see the definition of the word on " (a {:href "https://www.diki.pl/" :target "_blank"} "Diki.pl") ".")
    (h3 "Works on moble devices:")
-   (p "This player is designed to work on mobile devices as well as desktops. On devices with touch screens clicking on a word shows the grammatical analysis pop up, rather than playing the transcript from that point on.")))
+   (p "Care has been taken to make the interface work well on mobile and touch screen devices as well as in your desktop browser.")))
