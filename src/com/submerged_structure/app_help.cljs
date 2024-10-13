d31de7824c29eaa1b7f59f4abbbc50a545d72b11
James Pratt <me@jamiep.org>
2024-10-13 14:37:12 +0200

    updated app help

--- src/com/submerged_structure/app_help.cljs
+++ src/com/submerged_structure/app_help.cljs
@@ -7,9 +7,7 @@
   (fragment
    (h2 "How to use this player")
    (p "As the audio plays you will see the currently spoken word is highlighted.")
-   (h3 "Several transcripts available: ")
-   (p "At the very top of the page the heading with the transcript name is actually a dropdown menu, you can use this to switch between different transcripts.")
-
+   
    (h3 "Choose from several formatting options, by clicking on the tabs direcly below this message:")
    (div
         (p (b "\"Plain Transcript - No Coloring\"") " for no formatting. Click on any word to go to that word and begin playing.")
@@ -18,15 +16,20 @@
    (h3 "Playback control: ")
    (div
     (p "Hover over playback control buttons below the waveform with your cursor for more information about what they do.")
-    (p "When not in the \"Grammar X-Ray\" view, you can click on any word in the transcript to skip forward or backward to that word and begin playing.")
     (p "You can also click on the audio waveform itself to skip to that point in the audio.")
-    (p "The top of the waveform visualisation is a zoomed in view of the currently playing audio. Below this is a zoomed out view of the entire audio. You can click on either to skip to that point in the audio."))
-   (h3 "Translations: ")
+    (p "The top of the waveform visualisation is a zoomed in view of the currently playing audio. Below this is a zoomed out view of the entire audio. You can click on either to skip to that point in the audio.")
+    (dom/h4 "Keyboard Shortcuts:")
+    (dom/ul
+     (dom/li (dom/strong "a") " = back to beginning of sentence, repeatedly press to go to previous sentences.")
+     (dom/li (dom/strong "s") " = pause if playing; and play if paused.")
+     (dom/li (dom/strong "d") " = next sentence, repeatedly press to skip forward more than one sentence.")
+     (dom/li (dom/strong "w") " = toggle autopause after every sentence.")))
+   (h3 "Translations:")
    (div
-        (p "Click on the button next to a sentence to see a translation of that sentence. You can also click on the translation to hide it again.")
+        (p "Click on the grey button next to a sentence to see a translation of that sentence. You can also click on the translation to hide it again.")
         (p "You can also click on the button to the right of the player controls to show or hide all translations at once."))
    (h3 "Grammar Analysis:")
-   (p "When in the \"Grammar X-Ray\" view you can click any word to see the root word it is derived from.")
+   (p "Click any word to see the root word it is derived from.")
    (p "You can also click the eye button on the far right of the player controls to see all root words revealed.")
    (p "If you hover over the box  showing the root word, or on a touch device click on the box, a popup will further break down the grammatical features of the word.")
    (h3 "Links to dictionary for definitions of individual words:")
